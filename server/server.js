2ea45c5b1b0b50b2ba512298c423eb8f2db85deb
Julia <bullet.jul@gmail.com>
2023-10-15 15:31:37 +0300

    fix separate servers

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 import { graphqlServer } from './graphql_index.js';
 import { restApp } from './rest_index.js';
-import { connectionOptions, DB_URL, GRAPHQL_PORT, REST_PORT } from './src/config/config.js';
+import { connectionOptions, DB_URL, PORT } from './src/config/config.js';
 import { consoleInfo } from './src/utils/logger.js';
 
 (async () => {
@@ -9,9 +9,10 @@ import { consoleInfo } from './src/utils/logger.js';
         await mongoose.connect(DB_URL, connectionOptions);
         consoleInfo('Connected to DB');
 
-        await new Promise(resolve => graphqlServer.listen({ port: GRAPHQL_PORT }, resolve));
-        consoleInfo(`GRAPHQL Server listening on port http://localhost:${GRAPHQL_PORT}`);
+        await new Promise(resolve => graphqlServer.listen({ port: PORT }, resolve));
+        consoleInfo(`GRAPHQL Server listening on port http://localhost:${PORT}`);
 
+        const REST_PORT = Number(PORT) + 1;
         await new Promise(resolve => restApp.listen({ port: REST_PORT }, resolve));
         consoleInfo(`REST API Server listening on port http://localhost:${REST_PORT}`);
     } catch (err) {
