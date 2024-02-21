93e4946db24e3d9e240b328ecd26a39753fec807
Julia <bullet.jul@gmail.com>
2024-02-21 12:35:42 +0200

    on-click add

--- client/src/components/charts/DiagramBar.tsx
+++ client/src/components/charts/DiagramBar.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react'
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts'
+import { useNavigate } from 'react-router-dom'
 
 interface DiagramBarProps {
   chartData: { period: string; count: number }[]
@@ -7,10 +8,17 @@ interface DiagramBarProps {
 
 const DiagramBar: FC<DiagramBarProps> = (props) => {
   const { chartData } = props
+  const navigate = useNavigate()
+
+  const handleClickDate = (year?: string) => {
+    if (year) {
+      navigate(`/date/${year}`)
+    }
+  }
 
   return (
     <ResponsiveContainer width="95%" height="100%">
-      <BarChart data={chartData}>
+      <BarChart data={chartData} onClick={(e) => handleClickDate(e.activeLabel)}>
         <CartesianGrid stroke="#414951" strokeDasharray="25 25" />
         <XAxis dataKey="period" />
         <YAxis />
@@ -20,5 +28,6 @@ const DiagramBar: FC<DiagramBarProps> = (props) => {
     </ResponsiveContainer>
   )
 }
+// or Bar onClick={(e) => handleClickDate(e.period)}
 
 export default DiagramBar
