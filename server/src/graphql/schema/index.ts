565345a2cd9795c9dbc1778e31b8643f08fff1ab
Pulna Yuliia <95103780+bullet26@users.noreply.github.com>
2025-09-14 01:29:45 +0300

    ????

--- server/src/graphql/schema/index.ts
+++ server/src/graphql/schema/index.ts
@@ -4,6 +4,7 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
+console.log('__dirname', __dirname)
 
 const typesArray = loadFilesSync(path.join(__dirname, './*.graphql'))
 
