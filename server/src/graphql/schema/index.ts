b8935e308124d2f361ce03d27d69b1ac3920d92d
Pulna Yuliia <95103780+bullet26@users.noreply.github.com>
2025-09-14 11:40:06 +0300

    async

--- server/src/graphql/schema/index.ts
+++ server/src/graphql/schema/index.ts
@@ -1,12 +1,13 @@
-import { loadFilesSync } from '@graphql-tools/load-files'
+import { loadFiles } from '@graphql-tools/load-files'
 import { mergeTypeDefs } from '@graphql-tools/merge'
 import path from 'path'
 import { fileURLToPath } from 'url'
-import { consoleInfo } from '../../common/index.js'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
-consoleInfo(`__dirname ${__dirname}`)
 
-const typesArray = loadFilesSync(path.join(__dirname, '**/*.graphql'), { ignoreIndex: true })
+const typesArray = await loadFiles(path.join(__dirname, '*.graphql'), {
+  extensions: ['graphql'],
+  ignoreIndex: true,
+})
 
 export const typeDefs = mergeTypeDefs(typesArray)
