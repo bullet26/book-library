f9d9c18cc3cd54699e759d62d817815722aab066
Pulna Yuliia <95103780+bullet26@users.noreply.github.com>
2025-09-14 13:49:52 +0300

    test 129

--- server/src/graphql/schema/index.ts
+++ server/src/graphql/schema/index.ts
@@ -1,19 +1,11 @@
-import { loadFiles } from '@graphql-tools/load-files'
 import { loadSchema } from '@graphql-tools/load'
-
-import { mergeTypeDefs } from '@graphql-tools/merge'
-import path from 'path'
-
 import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader'
 
-// const typesArray = await loadFiles(path.join(__dirname, '*.graphql'), {
-//   ignoreIndex: true,
-//   extensions: ['graphql'],
-//   requireMethod: (path: any) => fs.readFileSync(path, 'utf-8'),
-// })
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-// export const typeDefs = mergeTypeDefs(typesArray)
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-export const typeDefs = await loadSchema(path.join(process.cwd(), 'graphql/schema/**/*.graphql'), {
+export const typeDefs = await loadSchema(path.join(__dirname, '*.graphql'), {
   loaders: [new GraphQLFileLoader()],
 })
