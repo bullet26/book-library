07532285afe2c0f23bf67d75a5a82e7f30db0cf6
Pulna Yuliia <95103780+bullet26@users.noreply.github.com>
2025-09-14 16:53:10 +0300

    test path

--- server/src/graphql/schema/index.ts
+++ server/src/graphql/schema/index.ts
@@ -1,17 +1,11 @@
 import { loadSchema } from '@graphql-tools/load'
 import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader'
-import { APP_MODE } from '../../config/index.js'
 
 import path from 'path'
 import { fileURLToPath } from 'url'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-const schemaPath =
-  APP_MODE === 'vercel'
-    ? path.join(process.cwd(), '**/*.graphql')
-    : path.join(__dirname, '*.graphql')
-
-export const typeDefs = await loadSchema(schemaPath, {
+export const typeDefs = await loadSchema(path.join(__dirname, '*.graphql'), {
   loaders: [new GraphQLFileLoader()],
 })
