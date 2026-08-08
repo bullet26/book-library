8a9afc0080403a2cd70a8ab146ccc3f3b941abb3
Pulna Yuliia <95103780+bullet26@users.noreply.github.com>
2026-08-08 23:08:33 +0300

    [protect route]

--- client/src/routes/routes.tsx
+++ client/src/routes/routes.tsx
@@ -12,15 +12,18 @@ import {
   BooksByTag,
   MostRededAuthors,
 } from 'pages'
+import { useReactContext } from 'providers'
 
 export const AppRoutes = () => {
+  const { isEditMode } = useReactContext()
+
   return (
     <Routes>
       <Route path="/" element={<MainPage />}>
         <Route path="home" element={<Home />} />
         <Route path="books" element={<Books />} />
         <Route path="books/:id" element={<Book />} />
-        <Route path="add" element={<AddBook />} />
+        {isEditMode && <Route path="add_book" element={<AddBook />} />}
         <Route path="date/:year" element={<BooksByDate />} />
         <Route path="tag" element={<BooksByTag />} />
         <Route path="most_reded_authors" element={<MostRededAuthors />} />
